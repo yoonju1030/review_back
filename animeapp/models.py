06852a51cf65b9a7ccabb6d973e9aeb0bb0b2795
yoonju1030 <allrealjgt@gmail.com>
2026-01-21 02:10:01 +0900

    모델 추가

--- animeapp/models.py
+++ animeapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import json
 
-# Create your models here.
+
+class Anime(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.URLField(max_length=500, blank=True, null=True)
+    air_year_quarter = models.CharField(max_length=20, blank=True, null=True)
+    content = models.TextField(blank=True, null=True)
+    content_rating = models.CharField(max_length=50, blank=True, null=True)
+    ended = models.BooleanField(default=False)
+    genres = models.JSONField(default=list, blank=True)
+    tags = models.JSONField(default=list, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "anime"
+
+    def __str__(self):
+        return self.name
